31607c7a1bc10d997350b519037d0a478ab5e9cc
karak <karak97@gmail.com>
2013-06-09 11:13:45 +0900

    Refined DB mapping layer.

--- js/db.js
+++ js/db.js
@@ -1,4 +1,4 @@
-var Database;
+var DatabaseConnection;
 
 (function () {
 var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB;
@@ -7,16 +7,19 @@ var IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || win
 var IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.mozIDBKeyRange;
 var Deferred = jQuery.Deferred; //dependency on jQuery; only jQuery.Deferred
 
-Database = function () {
+DatabaseConnection = function () {
+	this._db = null;
 };
 
-Database.prototype.open = function () {
+DatabaseConnection.prototype.open = function () {
+	var self = this;
 	var deferred = new Deferred();
 	var dbName = "test1", version = 2;
 
 	var reqOpen = indexedDB.open(dbName, version);
 
 	function upgrade (db, newVersion) {
+		//TODO: 外に出す
 		db.createObjectStore('productions', {keyPath: 'id', autoIncrement: false});
 	}
 
@@ -34,51 +37,110 @@ Database.prototype.open = function () {
 				upgrade(db, version);
 			};
 		}
+		self._db = db;
 		deferred.resolve();
 	};
 
 	reqOpen.onerror = function (err) {
+		self._db = null;
 		deferred.reject();
 	};
 
-	function Collection() {
-	}
-
-	Collection.prototype.save = function (data, options) {
-		options = options || {};
-		var deferred = new Deferred(function () {this.done(options.success); this.fail(options.error); });
-		var db = reqOpen.result;
-		var tx = options.transaction || db.transaction(['productions'], 'readwrite');
-		tx.oncomplete = function () {
-			deferred.resolve();
-		};
-		tx.onerror = function () {
-			deferred.reject();
-		};
-		var store = tx.objectStore('productions');
-		store.put(data);
-		return deferred.promise();
-	};
-
-	Collection.prototype.find = function (key, options) {
-		options = options || {};
-		var deferred = new Deferred(function () {this.done(options.success); this.fail(options.error); });
-		var db = reqOpen.result;
-		var tx = options.transaction || db.transaction(['productions'], 'readonly');
-		tx.oncomplete = function () {
-			deferred.resolve(reqGet.result);
-		};
-		tx.onerror = function () {
-			deferred.reject();
-		};
-		var store = tx.objectStore('productions');
-		var reqGet = store.get(key);
-		return deferred.promise();
-	};
-
-	this.productions = new Collection();
-
 	return deferred.promise();
+};
+
+DatabaseConnection.prototype.store = function (name) {
+	return new Store(this._db, name);
 }
+
+function Store(db, name) {
+	this._db = db;
+	this._name = name;
+}
+
+Store.prototype.save = function (data, options) {
+	options = options || {};
+	var self = this;
+	var deferred = new Deferred(function () {this.done(options.success); this.fail(options.error); });
+	var db = self._db;
+	var tx = options.transaction || db.transaction([self._name], 'readwrite');
+	tx.oncomplete = function () {
+		deferred.resolve();
+	};
+	tx.onerror = function () {
+		deferred.reject();
+	};
+	var store = tx.objectStore(self._name);
+	store.put(data);
+	return deferred.promise();
+};
+
+Store.prototype.find = function (key, options) {
+	options = options || {};
+	var self = this;
+	var deferred = new Deferred(function () {this.done(options.success); this.fail(options.error); });
+	var db = self._db;
+	var tx = options.transaction || db.transaction([self._name], 'readonly');
+	tx.oncomplete = function () {
+		deferred.resolve(reqGet.result);
+	};
+	tx.onerror = function () {
+		deferred.reject();
+	};
+	var store = tx.objectStore(self._name);
+	var reqGet = store.get(key);
+	return deferred.promise();
+};
+} ());
+
+var Repository;
+
+(function () {
+/********************************/
+/* Generic Repository           */
+/********************************/
+
+/**
+ * @param {DatabaseConnection} conn
+ * @param {String} storeName
+ * @param {Function} constructor
+ */
+var Collection = function (conn, storeName, constructor) {
+  this._conn = conn;
+  this._storeName = storeName;
+  this._constructor = constructor;
+};
+
+Collection.prototype.load = function (key) {
+  var self = this;
+  var deferred = new $.Deferred();
+  var req = self._conn.store(self._storeName).find(key);
+
+  req.done(function (data) {
+    var obj = new self._constructor(data);
+    deferred.resolve(obj);
+  });
+
+  req.fail(function (err) {
+    deferred.reject(err);
+  });
+
+  return deferred.promise();
+};
+
+//export
+Repository = function (collections) {
+  var self = this;
+  var conn = new DatabaseConnection();
+  self._conn = conn;
+
+  $.each(collections, function (name, constructor) {
+    self[name] = new Collection(conn, name, constructor);
+  });
+};
+
+Repository.prototype.open = function () {
+  return this._conn.open();
+};
 
 } ());
